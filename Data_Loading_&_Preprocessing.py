e7d76f10f5bee93210a6ee15d40fbeca621de152
Slayerma <syedmohathashimali93@gmail.com>
2024-09-17 21:31:30 +0530

    Data Processing

---
+++ Data_Loading_&_Preprocessing.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def load_and_explore_data(data_path):
+    # Load the dataset
+    data = pd.read_csv(data_path)
+
+    # Display the first few rows
+    print(data.head())
+    print(data.info())
+    # Basic statistics
+    print(data.describe())
+    # Check for missing values
+    print(data.isnull().sum())
+
+    data['date'] = pd.to_datetime(data['date'], format='%d/%m/%Y %H:%M')
+
+    print(data['date'].head)
+    print(data.duplicated().sum())
+    data = data.drop_duplicates()
+
+    # Plot outliers in 'Usage_kWh'
+    plt.boxplot(data['Usage_kWh'])
+    plt.title('Usage_kWh Outliers')
+    plt.show()
+
+    # Extract hour, month, and day from 'date'
+    data['hour'] = data['date'].dt.hour
+    data['month'] = data['date'].dt.month
+    data['day'] = data['date'].dt.day
